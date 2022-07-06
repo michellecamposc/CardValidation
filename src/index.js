6b696a9bfc09c2a56b4e695570ae0330cb543838
michellecamposc <stephaniecampoos95@gmail.com>
2022-07-06 07:49:20 -0400

    Actualización con javascript

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
-import validator from './validator.js';
+import validator from "./validator.js";
 
-console.log(validator);
+// Llamar al input desde html
+function creditCardChanges() {
+  const cardNumberInput = document.getElementById("card-number");
+  console.log(cardNumberInput);
+  cardNumberInput.addEventListener("input", verify);
+}
+
+// Verificar
+function verify(event) {
+  console.log(event);
+  const value = event.srcElement.value;
+  console.log(value);
+  const isValid = validator.isValid(value);
+  const messageHtml = document.getElementById("message");
+
+  if (isValid) {
+    messageHtml.style.color = "seagreen";
+    messageHtml.innerText =
+      "✅ Tarjeta de crédito válida " + validator.maskify(value);
+  } else {
+    messageHtml.style.color = "red";
+    messageHtml.innerText = "❌ Tarjeta no válida";
+  }
+}
+
+creditCardChanges();
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,40 @@
 const validator = {
-  // ...
+  isValid: function (creditCardNumber) {
+    const creditCardNumbersList = creditCardNumber
+      .split("")
+      .reverse()
+      .map((x) => +x);
+
+    const lastDigit = creditCardNumbersList.splice(0, 1)[0];
+    let sum = creditCardNumbersList.reduce(
+      (acc, val, i) => (i % 2 !== 0 ? acc + val : acc + ((val * 2) % 9) || 9),
+      0
+    );
+    sum += lastDigit;
+    return sum % 10 === 0 ? true : false;
+  },
+
+  /**
+   * Returns all the characters except the last four, whose are replaced by a cat emoji (🐱)
+   * Should always keep the last four characters, even when the creditCardNumber param is shorter
+   */
+  maskify: function (creditCardNumber) {
+    let masked = "";
+    for (let i = 0; i < creditCardNumber.length; i++) {
+      if (i < creditCardNumber.length - 4) masked += "🐱";
+      else masked += creditCardNumber[i];
+    }
+
+    return masked;
+  },
 };
 
 export default validator;
+
+/**
+ * console.log(validator.isValid("5181975718047403")); // === true
+ * console.log(validator.maskify("5181975718047403")); // === '🐱🐱🐱🐱🐱🐱🐱🐱🐱🐱🐱🐱7403'
+ * console.log(validator.maskify("75718047403")); // === '🐱🐱🐱🐱🐱🐱🐱7403'
+ * console.log(validator.maskify("3")); // === '3'
+ * console.log(validator.maskify("")); // === ''
+ */
